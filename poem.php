698d809365173314ffddb5bc7333ca6e9111a548
miwanoff <marina.iva.noffa@gmail.com>
2024-11-16 13:25:21 +0200

    Add poem

---
+++ poem.php
@@ -0,0 +1,28 @@
+<?php
+
+$str_b = '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>'."\n\t";
+
+$str_e = "\n".'</body>
+</html>';
+
+$f = fopen("poem.txt", "r");
+
+$arr = file("poem.txt");
+
+$content = "<h1>Poem</h1>";
+
+$h = fopen("poem.html", "w");
+
+fwrite($h, $str_b);
+fwrite($h, $content);
+fwrite($h, $str_e);
+
+fclose($f);
+fclose($h);
